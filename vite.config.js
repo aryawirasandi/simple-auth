f30df7228769d6471b22933dc083fa799ecf0c9c
aryawirasandi <aryawirasandi7153@gmail.com>
2023-02-18 23:17:40 +0800

    remove vitest and add jest

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,4 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-    globals: true,
-    environment: 'jsdom',
-  }
 })
