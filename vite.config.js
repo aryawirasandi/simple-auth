1def3a0392d044ca5a1099de66766e9efc0e453e
aryawirasandi <aryawirasandi7153@gmail.com>
2023-02-24 00:21:19 +0800

    install auto import plugin

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-
+import AutoImport from "unplugin-auto-import/vite"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
