bf86435ac15e02c4ecfa2de328fcdde1887199e5
aryawirasandi <aryawirasandi7153@gmail.com>
2023-02-24 00:05:47 +0800

    add auto import library and components

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,28 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-    globals: true,
-    environment: 'jsdom',
-  }
+  plugins: [
+    react(),
+    AutoImport({
+      include: [
+        /\.[tj]sx?$/,
+      ],
+      imports: [
+        {
+          'react': [
+            'useEffect',
+            'useState'
+          ]
+        }
+      ],
+      defaultExportByFilename: true,
+      dirs: [
+        'src/**',
+        'src/components',
+        'src/pages'
+      ],
+      cache: false,
+      reactComponent: true
+    })
+  ],
 })
