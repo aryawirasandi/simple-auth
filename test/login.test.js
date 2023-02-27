74facba7045c8ababc9d21aff11b7823e0addef0
arya_wirasandi <arya.wirasandi@salt.co.id>
2023-02-27 21:10:57 +0800

    add two todo scenario testing

---
+++ test/login.test.js
@@ -0,0 +1,9 @@
+
+
+describe("Testing Login Component", () => {
+    it.todo('it can input username')
+    it.todo('it can input password')
+    it.todo('it will show an error')
+    it.todo('cant submit if one of form is null')
+    it.todo('click button below redirect to the register page')
+});
---
+++ test/register.test.js
@@ -0,0 +1,8 @@
+describe("Testing Login Component", () => {
+    it.todo('it can input username')
+    it.todo('it can input password')
+    it.todo('it can retype password')
+    it.todo('it will show an error if one of the form missed')
+    it.todo('cant submit if one of form is null')
+    it.todo('click button below redirect to the login page page')
+});
