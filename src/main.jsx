6de2cbdbb25f605ecbe59bd8e3e2bc6620c553e4
arya_wirasandi <arya.wirasandi@salt.co.id>
2023-02-28 18:53:21 +0800

    remove App in the main

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom';
-import App from './App'
 import './assets/index.css'
 import router from "./routes/index";
 ReactDOM.createRoot(document.getElementById('root')).render(
   <RouterProvider router={router}>
-    <App />
   </RouterProvider>
 )
